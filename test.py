7043d4ae47fdc0972db54c7ba2d69fe6a49055b7
KayZinPhone <44814583+KayZinPhone@users.noreply.github.com>
2018-11-20 09:44:31 +0800

    input username

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
 name = input("Enter name: ")
+while len(name) <= 0:
+    print("Your name is blank, enter aganin: ")
+    name = input("Enter name: ")
+
 print(name[1:3])
+print(name[1:3:2])
+for i in range(0, len(name), 2):
+    print(name[i])
